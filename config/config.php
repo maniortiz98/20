afb86d6e0ddc9aa78d3fb82dbaca1f0dc01de234
mani <maniortiz98@gmail.com>
2020-06-19 20:49:38 -0500

    20

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php 
+/*dos formas para variables constantes */
+    /*define("BASE_URL", "http://localhost/tienda_virtual/" );*/
+    const BASE_URL = "http://localhost/tienda_virtual/";
+
+   //db
+    const DB_HOST= "localhost";
+    const DB_NAME= "tienda";
+    const DB_USER= "root";
+    const DB_PASSWORD= "";
+    const DB_CHARSET ="charset=utf8";  
+?>
